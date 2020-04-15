04663868893fbc7bbc3932846a799f15a64edbf5
Nathan <nathankirley@gmail.com>
2020-04-15 16:22:32 +0100

    albums save method

--- models/albums.rb
+++ models/albums.rb
@@ -12,5 +12,19 @@ class Albums
         @artist_id = options['artist_id'].to_i
     end
 
+    def save()
+        sql = "INSERT INTO albums
+        (
+            title,
+            genre,
+            artist_id
+            ) 
+            VALUES
+            ($1, $2, $3)
+            RETURNING id"
+            values = [@title, @genre, @artist_id]
+            result = SqlRunner.run(sql, values)
+            @id = result[0]['id'].to_i
+    end
 
 end
