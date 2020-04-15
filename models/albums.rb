db4eeaa28c71847f38b6efe7aa70de2084460ffb
Nathan <nathankirley@gmail.com>
2020-04-15 16:27:11 +0100

    albums list all method

--- models/albums.rb
+++ models/albums.rb
@@ -27,4 +27,10 @@ class Albums
             @id = result[0]['id'].to_i
     end
 
+    def self.all()
+        sql = "SELECT * FROM albums"
+        albums = SqlRunner.run(sql)
+        return albums.map {|album| Albums.new(album)}
+    end
+
 end
